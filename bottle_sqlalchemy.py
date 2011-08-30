d183bb08d6d675c0d9a4ec0c46821b31890ed6cc
Iuri de Silvio <iurisilvio@gmail.com>
2011-08-30 10:01:46 -0300

    Changed to capture only SQLAlchemyErrors instead all Exceptions.

--- bottle_sqlalchemy.py
+++ bottle_sqlalchemy.py
@@ -1,10 +1,63 @@
+'''
+This bottle_sqlalchemy plugin integrates SQLAlchemy with your Bottle
+application. It connects to a database at the beginning of a request,
+passes the database handle to the route callback and closes the connection
+afterwards.
+
+It inject an argument to all route callbacks that require a `db` keyword.
+
+Usage Example::
+
+    import bottle
+    from bottle import HTTPError
+    from bottle.ext import sqlalchemy
+    from sqlalchemy import create_engine, Column, Integer, Sequence, String
+    from sqlalchemy.ext.declarative import declarative_base
+
+    Base = declarative_base()
+    engine = create_engine('sqlite:///:memory:', echo=True)
+
+    app = bottle.Bottle()
+    plugin = sqlalchemy.Plugin(engine, Base.metadata)
+    app.install(plugin)
+
+    class Entity(Base):
+        __tablename__ = 'entity'
+        id = Column(Integer, Sequence('id_seq'), primary_key=True)
+        name = Column(String(50))
+
+        def __init__(self, name):
+            self.name = name
+
+        def __repr__(self):
+            return "<Entity('%d', '%s')>" % (self.id, self.name)
+
+    @app.get('/:name')
+    def show(name, db):
+        entity = db.query(Entity).filter_by(name=name).first()
+        if entity:
+            return {'id': entity.id, 'name': entity.name}
+        return HTTPError(404, 'Entity not found.')
+
+    @app.put('/:name')
+    def put_name(name, db):
+        entity = Entity(name)
+        db.add(entity)
+'''
+
+__author__ = "Iuri de Silvio"
+__version__ = '0.1'
+__license__ = 'MIT'
+
+### CUT HERE (see setup.py)
+
 import bottle
 from bottle import HTTPError
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.exc import SQLAlchemyError
 
 class SQLAlchemyPlugin(object):
 
@@ -12,7 +65,7 @@ class SQLAlchemyPlugin(object):
 
     def __init__(self, engine, metadata=None, keyword='db', create=True, autocommit=True):
         if create and not metadata:
-            raise TypeError('To create database, define metadata value.')
+            raise TypeError('Define metadata value to create database.')
         self.engine = engine
         self.metadata = metadata
         self.keyword = keyword
@@ -46,7 +99,7 @@ class SQLAlchemyPlugin(object):
                 rv = callback(*args, **kwargs)
                 if self.autocommit:
                     session.commit()
-            except Exception, e:
+            except SQLAlchemyError, e:
                 session.rollback()
                 raise HTTPError(500, "Database Error", e)
             finally:
